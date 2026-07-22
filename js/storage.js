a35de4bd35ef0620a3e73311e27cbe0bf9138c58
J. Gabriel Corpas <naoxink@icloud.com>
2026-07-22 23:44:45 +0200

    Actualizar storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
 export const Storage = {
-    get: (key, fallback) => JSON.parse(localStorage.getItem(key)) ?? fallback,
+    get: (key, fallback) => JSON.parse((localStorage.getItem(key) || '{}')) ?? fallback,
     set: (key, val) => localStorage.setItem(key, JSON.stringify(val))
 };
